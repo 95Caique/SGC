b842bc17540704491e065898389cad9f72a8fc1c
caique <decaiquesouza@gmail.com>
2026-04-26 15:26:09 -0300

    FEATURE: Criação das apps do projeto

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
+    'companies'
+    'clients',
+    'contracts',
+    'proposals',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
